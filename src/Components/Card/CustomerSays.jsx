bbf131c18a0a57b54e24ae7c1831a7b01f1e1106
Gurudas-Bhardwaj <163430325+Gurudas-Bhardwaj@users.noreply.github.com>
2024-05-15 11:44:31 +0530

    Add files via upload

---
+++ src/Components/Card/CustomerSays.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function CustomerSays(props) {
+  return (
+    <div>
+      <div className=' h-56 bg-slate-100 rounded-lg  w-72 md:w-80  flex flex-col'>
+            <span className='pl-4 pt-3'>{props.Stars}</span>
+            <span className='pl-4 pt-2 font-Popins pr-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi officia eaque blanditiis minus suscipit veniam!</span>
+            <div className='pl-4 pt-5 flex gap-3'>
+                <img src={props.Image} alt="" className='h-9 w-9 rounded-full' />
+                <h1 className='font-Popins pt-1 text-xl '>{props.Title}</h1>
+            </div>
+      </div>
+    </div>
+  )
+}
+
+export default CustomerSays
